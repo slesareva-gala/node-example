98e51165729e80d7216e900feb2f15331fdb1e4b
Gala Slesareva <galaslesareva@gmail.com>
2026-02-08 20:14:46 +0300

    Задания урока 4

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-console.log('Hello NPM');
+import { EventEmitter } from 'node:events';
+
+// ЗАДАНИЕ 1
+class TimerTick extends EventEmitter {
+  constructor() {
+    super();
+    this.tick = 0;
+    this.id = -1;
+  }
+  start(ms = 1000) {
+    this.stop(this.id);
+    this.id = setInterval(() => {
+      this.tick++;
+      this.show();
+    }, ms)[Symbol.toPrimitive]();
+  }
+
+  show() {
+    console.log(`Tick - ${this.tick}`);
+  }
+
+  stop() {
+    if (this.id > -1) {
+      clearInterval(this.id);
+      this.tick = 0;
+      this.id = -1;
+    }
+  }
+}
+
+const timerTick = new TimerTick();
+
+timerTick.start(500);
+
+setTimeout(() => {
+  timerTick.start();
+}, 2500);
+
+setTimeout(() => {
+  timerTick.stop();
+}, 7000);
+
+// ЗАДАНИЕ 2
+class Messager extends EventEmitter {}
+const messager = new Messager();
+
+const receiveMessage = ({ userName, message }) => {
+  console.log(`${userName}: ${message}`);
+};
+messager.on('mess', receiveMessage);
+
+const sendMessage = (userName, message) => {
+  messager.emit('mess', { userName, message });
+};
+
+setTimeout(() => {
+  sendMessage('Node', 'Hello, Gala!');
+}, 2050);
+setTimeout(() => {
+  sendMessage('Gala', 'Hello, Node!');
+}, 2100);
