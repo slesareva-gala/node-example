a897364e2779ce10d70e2ae13885ee2cd01bc75f
Gala Slesareva <galaslesareva@gmail.com>
2026-02-24 03:30:51 +0300

    задание 3 урока 6

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hello NPM');
+import { createWriteStream, createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import sharp from './modules/sharp.cjs';
+
+async function resizeImage(inputPath, outputPath) {
+  try {
+    await pipeline(
+      createReadStream(inputPath),
+      sharp().resize(400, 400),
+      createWriteStream(outputPath),
+    );
+  } catch (e) {
+    console.error('resizeImage: ', e.message);
+  }
+}
+
+async function blurredGgrayImage(inputPath, outputPath) {
+  try {
+    await pipeline(
+      createReadStream(inputPath),
+      sharp().greyscale().blur(3),
+      createWriteStream(outputPath),
+    );
+  } catch (e) {
+    console.error('blurredGgrayImage: ', e.message);
+  }
+}
+
+resizeImage('./files/horse.jpeg', './files/horseMini.jpeg');
+blurredGgrayImage('./files/horse.jpeg', './files/horseBG.jpeg');
